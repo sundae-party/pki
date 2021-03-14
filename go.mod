5eb0e05b5dc58efdbca1cd26639958b382d91244
Cyril connan <cyril.connan@gmail.com>
2021-03-14 15:57:33 +0000

    Add utils function to build SSL server conf and SSL gRPC client conf

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.0
+	google.golang.org/grpc v1.21.1
 )
--- utils/cert.go
+++ utils/cert.go
@@ -1,17 +1,26 @@
 package utils
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"crypto/x509/pkix"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net"
 	"os"
 	"time"
+
+	"google.golang.org/grpc/credentials"
 
 	"github.com/sundae-party/pki/ca"
 	"github.com/sundae-party/pki/csr"
 )
 
+//CreateCertFromCAFile create a new certificate and private key.
+// That can be used at the server side for TLS server configuration and
+// at the client side for the mTLS authentication
 func CreateCertFromCAFile(caKeyPath string, caCertPath string, cn string, duration time.Duration, sansDns []string, sansIp []net.IP, dest string, certFileName string, keyFileName string) error {
 
 	// Load CA
@@ -53,4 +62,68 @@ func CreateCertFromCAFile(caKeyPath string, caCertPath string, cn string, durati
 	}
 
 	return nil
+}
+
+// BuildServerTlsConf create a tlsConfig object of type *tls.Config configured to be used in the server side.
+// If one or more CA certificates are provided through CAPaths,
+// mTLS configuration will be enabled and this certificates will be used to validate the client certificates.
+func BuildServerTlsConf(CAPaths []string, certPath string, keyPath string) (tlsConfig *tls.Config, err error) {
+
+	// SSL server configuration
+	serverCert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, err
+	}
+	tlsConfig = &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+	}
+
+	// mTLS configuration
+	if len(CAPaths) > 0 {
+		caCertPool := x509.NewCertPool()
+
+		// Create a CA certificate pool with all CAs used to sign client certificates for the mTLS.
+		for _, caPath := range CAPaths {
+			caCert, err := ioutil.ReadFile(caPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			caCertPool.AppendCertsFromPEM(caCert)
+		}
+
+		// Create the server TLS Config with the CA pool and enable Client certificate validation.
+		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+		tlsConfig.ClientCAs = caCertPool
+	}
+
+	return tlsConfig, nil
+}
+
+// LoadKeyPair create a tlsConfig object of type credentials.TransportCredentials configured to be used in the gRPC client side with mTLS enabled.
+func LoadKeyPair(certPath string, keyPath string, caPath string) (clientTLSConfig credentials.TransportCredentials, err error) {
+
+	// Load client cert & key
+	certificate, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Load CA
+	ca, err := ioutil.ReadFile(caPath)
+	if err != nil {
+		return nil, err
+	}
+
+	capool := x509.NewCertPool()
+	if !capool.AppendCertsFromPEM(ca) {
+		return nil, errors.New("Cann't add ca")
+	}
+
+	// Build TLS config
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      capool,
+	}
+
+	return credentials.NewTLS(tlsConfig), nil
 }
